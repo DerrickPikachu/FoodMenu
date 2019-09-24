19ebd20c90d66d06a49fa1b2fb59ad881123f2ad
pikaChin <derrfgh4563@gmail.com>
2019-09-24 10:59:44 +0800

    ordering complete

--- app/src/main/java/com/example/foodmenu/MainActivity.java
+++ app/src/main/java/com/example/foodmenu/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.foodmenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView resultPrint;
+    CheckBox boxes[];
+    Button orderBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boxes = new CheckBox[4];
+        resultPrint = findViewById(R.id.result);
+        orderBtn = findViewById(R.id.orderBtn);
+        boxes[0] = findViewById(R.id.hamburger);
+        boxes[1] = findViewById(R.id.frenchFries);
+        boxes[2] = findViewById(R.id.cola);
+        boxes[3] = findViewById(R.id.cornSoup);
+
+        orderBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String result = "你點購的餐點是:\n";
+
+        for (int i=0; i<4; i++) {
+            if (boxes[i].isChecked())
+                result = result + boxes[i].getText() + "\n";
+        }
+
+        resultPrint.setText(result);
     }
 }
