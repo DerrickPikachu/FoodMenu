d5a12fae381d6971337d3f1cefe46486268f42df
pikaChin <derrfgh4563@gmail.com>
2019-09-27 00:18:40 +0800

    dynamically update result

--- app/src/main/java/com/example/foodmenu/MainActivity.java
+++ app/src/main/java/com/example/foodmenu/MainActivity.java
@@ -6,13 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
 
-    TextView resultPrint;
-    int boxesId[] = {R.id.hamburger, R.id.frenchFries, R.id.cola, R.id.cornSoup, R.id.friedChicken, R.id.coffee};
-    Button orderBtn;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+
+    private TextView resultPrint;
+    private int boxesId[] = {R.id.hamburger, R.id.frenchFries, R.id.cola, R.id.cornSoup, R.id.friedChicken, R.id.salad, R.id.coffee, R.id.applePie};
+    private Button orderBtn;
+    private ArrayList<String> checked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +25,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         resultPrint = findViewById(R.id.result);
         orderBtn = findViewById(R.id.orderBtn);
+        checked = new ArrayList<>();
 
         orderBtn.setOnClickListener(this);
+
+        for (int i:boxesId) {
+            CheckBox box = findViewById(i);
+            box.setOnCheckedChangeListener(this);
+        }
     }
 
     @Override
     public void onClick(View view) {
-        String result = "你點購的餐點是:\n";
+        String result = "您訂購的餐點是:\n";
         CheckBox box;
 
         for (int i:boxesId) {
@@ -35,6 +45,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if (box.isChecked())
                 result = result + box.getText() + "\n";
         }
+
+        resultPrint.setText(result);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        String result = "您點的餐點是:\n";
+
+        if (b)
+            checked.add(compoundButton.getText().toString());
+        else
+            checked.remove(compoundButton.getText().toString());
+
+        for (int i=0; i<checked.size(); i++)
+            result = result + checked.get(i) + "\n";
 
         resultPrint.setText(result);
     }
